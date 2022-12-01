b7775ce673dbd046670df7d46d44e596e64ffd99
Alex Davis <8m3xlt@gmail.com>
2022-12-01 20:25:19 +0000

    one-liners

--- 1.py
+++ 1.py
@@ -4,3 +4,9 @@ sums = [sum([int(i) for i in line.split()]) for line in lines]
 print(sums)
 print(max(sums))
 print(sum(sorted(sums, reverse=True)[:3]))
+
+# part 1 one-liner - ignore that it doesn't close the file, it's fine
+print(max([sum([int(i) for i in line.split()]) for line in open("1.txt", "r").read().split("\n\n")]))
+
+# part 2 one-liner
+print(sum(sorted([sum([int(i) for i in line.split()]) for line in open("1.txt", "r").read().split("\n\n")], reverse=True)[:3]))
