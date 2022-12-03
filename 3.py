e5830269651543720243a986685c8d20f9d6fc99
Alex Davis <8m3xlt@gmail.com>
2022-12-03 15:08:46 +0000

    day 3 part 2

--- 3.py
+++ 3.py
@@ -1,13 +1,12 @@
 items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 with open("3.txt", "r") as file:
-    rucksacks = [[i[:len(i)//2], i[len(i)//2:]] for i in file.read().splitlines()]
-
-
+    lines = file.read().splitlines()
+    groups = [[lines[i], lines[i+1], lines[i+2]] for i in [j for j in range(len(lines))][::3]]
 common_items = []
-for compartments in rucksacks:
-    for item in compartments[0]:
-        if item in compartments[1]:
+for group in groups:
+    for item in group[0]:
+        if item in group[1] and item in group[2]:
             common_items.append(item)
             break
 
